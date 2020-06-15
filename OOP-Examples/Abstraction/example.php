a96e275cc3fb38fe2372d787a3a6a6e944b6663c
Noah <osuolale49@gmail.com>
2020-06-15 08:42:55 +0100

    The Pillars of OOP and Principles of SOLID

--- OOP-Examples/Abstraction/example.php
+++ OOP-Examples/Abstraction/example.php
@@ -1,29 +1,36 @@
 <?php  
-abstract class Animal  
-{  
+
+abstract class Animal {  
 public $name;  
 public $age;  
-public function Describe()  
-{  
-return $this->name . ", " . $this->age . " years old";      
-        }  
-abstract public function Greet();  
+
+  public function Describe()  
+  {  
+  return $this->name . ", " . $this->age . " years old";      
+  }  
+
+  abstract public function Greet();  
+  }  
+
+class cat extends Animal  {
+  
+  public function Greet()  
+  {  
+    return "Lion!";      
+  }  
+
+  public function Describe()  
+  {  
+    return parent::Describe() . ", and I'm a cat!";      
+  }  
 }  
-class cat extends Animal  
-{  
-public function Greet()  
-{  
-return "Lion!";      
-}  
-public function Describe()  
-{  
-return parent::Describe() . ", and I'm a cat!";      
-}  
-}  
+
 $animal = new cat();  
+
 $animal->name = "Seru";  
 $animal->age = 5;  
+
 echo $animal->Describe();  
-echo $animal->Greet();  
+// echo $animal->Greet();  
   
  
